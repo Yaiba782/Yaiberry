27128f2e3324ab4c944800453e2639faaeab58ff
Yaiba <yaiba782@gmail.com>
2016-02-24 09:53:29 +0100

    Again

--- index.php
+++ index.php
@@ -2,11 +2,11 @@
     <head>
         <title>Welcome !</title>
         <meta charset="UTF-8">
-        <link rel="shortcut icon" href="/favicon.ico" type="image/icon">
-        <link rel="icon" href="/favicon.ico" type="image/icon">
-        <link rel="stylesheet" href="/includes/css/style.css">
-        <link rel="stylesheet" href="/includes/bootstrap/bootstrap.min.css">
-        <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto" type="text/css">
+        <link rel="shortcut icon" href="/favicon.ico" type="image/x-icon">
+        <link rel="icon" href="/favicon.ico" type="image/x-icon">
+        <link rel="stylesheet" href="includes/css/style.css">
+        <link rel="stylesheet" href="includes/bootstrap/bootstrap.min.css">
+        <link rel='stylesheet' href='https://fonts.googleapis.com/css?family=Roboto' type='text/css'>
     </head>
     <body class="content">';
     <?php include_once("analyticstracking.php"); ?>
