5c759a613d2a05ded58fd93518027ba82ec4aa5d
Yaiba <yaiba782@gmail.com>
2016-02-24 09:48:14 +0100

    Edit head.php

--- includes/head.php
+++ includes/head.php
@@ -1,13 +1,14 @@
-<html>
-<head>
-    <title>Welcome !</title>
-    <meta charset="UTF-8">
-    <link rel="shortcut icon" href="/favicon.ico" type="image/icon">
-    <link rel="icon" href="/favicon.ico" type="image/icon">
-    <link rel="stylesheet" href="/includes/css/style.css">
-    <link rel="stylesheet" href="/includes/bootstrap/bootstrap.min.css">
-    <link rel='stylesheet' href='https://fonts.googleapis.com/css?family=Roboto' type='text/css'>
-</head>
-<body class="content">
+<?php
+    echo '<html>
+            <head>
+                <title>Welcome !</title>
+                <meta charset="UTF-8">
+                <link rel="shortcut icon" href="/favicon.ico" type="image/icon">
+                <link rel="icon" href="/favicon.ico" type="image/icon">
+                <link rel="stylesheet" href="/includes/css/style.css">
+                <link rel="stylesheet" href="/includes/bootstrap/bootstrap.min.css">
+                <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto" type="text/css">
+            </head>
+            <body class="content">';
 
-<?php include_once("analyticstracking.php") ?>
+    include_once("analyticstracking.php");
