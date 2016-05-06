4b08a0dabe0e1486752c8ca8f04b02e8ea00f84f
Yaiba <yaiba782@gmail.com>
2016-05-06 10:55:24 +0200

    Fetching coinmarket API and displaying the value

--- bitcoin.php
+++ bitcoin.php
@@ -1,137 +1,90 @@
-WIP !
 <?php
+if(isset($_GET['d'])){
     /*
-    // Fetching API
-    $ethApi = file_get_contents('https://coinmarketcap-nexuist.rhcloud.com/api/eth');
-    $btcApi = file_get_contents('https://coinmarketcap-nexuist.rhcloud.com/api/btc');
+     *
+     *  Fetching API
+     *
+     * */
+    $ethApi = file_get_contents('https://coinmarketcap-nexuist.rhcloud.com/api/eth/price');
+    $btcApi = file_get_contents('https://coinmarketcap-nexuist.rhcloud.com/api/btc/price');
 
     // Decoding the 'json' string
     $btcJson = json_decode($btcApi);
     $ethJson = json_decode($ethApi);
 
-    // BTC
-    $eurToBtc = $btcJson->price->eur;
-    $usdToBtc = $btcJson->price->usd;
-
-    // ETH
-    $eurToEth = $ethJson->price->eur;
-    $usdToEth = $ethJson->price->usd;
-    $btcToEth = $ethJson->price->btc;
-
 ?>
 <html>
-    <head>
-        <title>Welcome !</title>
-        <meta charset="UTF-8">
-        <link rel="shortcut icon" href="/favicon.ico" type="image/x-icon">
-        <link rel="icon" href="/favicon.ico" type="image/x-icon">
-        <link rel="stylesheet" href="includes/css/style.css">
-        <link rel="stylesheet" href="includes/bootstrap/bootstrap.min.css">
-        <link rel='stylesheet' href='https://fonts.googleapis.com/css?family=Roboto' type='text/css'>
-    </head>
-    <body class="content">
+
+<head>
+    <title>Aproximacoin !</title>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+    <link rel="stylesheet" href="assets/css/main.css" />
+</head>
+<body>
+    <!-- Analytics script -->
     <script>
- //       (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-   //             (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-     //       m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-       // })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
-
-//        ga('create', 'UA-74196136-1', 'auto');
-//        ga('send', 'pageview');
-
+    (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+        (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+        m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+    })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+    ga('create', 'UA-74196136-1', 'auto');
+    ga('send', 'pageview');
     </script>
-        <div class="navbar col-xs-12 col-md-10 col-md-offset-1">
-            <div class="title col-xs-10 col-xs-offset-1 col-lg-3 col-lg-offset-1">
-                <h1>
-                    <a class="home" href="./">Yaiba</a>
-                </h1>
-            </div>
-            <div class="col-lg-3 col-lg-offset-5">
-                <a target="_blank" href="http://github.com/Yaiba782/Yaiberry">C'est si moche que ça ?</a>
-            </div>
+
+    <!-- Prices -->
+    <div data-btceur="<?php echo $btcJson->eur; ?>" data-etheur="<?php echo $ethJson->eur; ?>" data-btcusd="<?php echo $btcJson->usd; ?>" data-ethusd="<?php echo $ethJson->usd; ?>"></div>
+    <!-- Header -->
+    <section id="header">
+        <header>
+            <h1>Aproximacoin</h1>
+        </header>
+        <footer>
+        <p>
+            Welcome to the new version of the Aproximacoin.<br />
+            You can convert Euro, US Dollar to Bitcoin, and Ether.
+        </p>
+            <a href="#banner" class="button style2 scrolly">Let's go</a>
+        </footer>
+    </section>
+
+    <section id="banner" class="aproximacoin_banner">
+        <div class="col-xs-offset-4 col-xs-4">
+            <select name="realCurrency">
+                <option value="eur" selected >EUR</option>
+                <option value="usd" >USD</option>
+            </select>
         </div>
-    <div class="wrap">
-
-        <div class="row col-lg-12 center">
-                <h1>Welcome to the Aproximacoin !</h1>
-                You understood, it gives you a (pretty accurate) aproximation of the market price for the Bitcoin !<br />
-                (source : Blockchain.info)<br />
-                (source : Coinmarketcap.com)
-            </div>
-            <div class="col-lg-12 nbsp"></div>
-            <div class="data" data-eurToeth="<?php echo $eurToEth; ?>" data-usdToEth="<?php echo $usdToEth; ?>" data-btcToEth="<?php echo $btcToEth; ?>"  data-eurToBtc="<?php echo $eurToBtc; ?>" data-usdToBtc="<?php echo $usdToBtc; ?>"></div>
-            <div class="col-lg-7 col-lg-offset-2 col-xs-10 col-xs-offset-1 center">
-                <select name="currency" id="currency">
-                    <option value="eurtobtc">EUR</option>
-                    <option value="usdtobtc">USD</option>
-                </select>
-                <input type="number" id="input1" value="1"><span id="valueIcon" class="glyphicon glyphicon-euro"></span>
-                    =
-                <input type="number" id="input2" value="<?php echo $eurToBtc; ?>">
-                <span class="glyphicon glyphicon-bitcoin"></span>
-                    =
-                <input type="number" id="input4" value="<?php echo round($eurToEth,3); ?>">
-                <img class="eth-logo" src="./images/ETHEREUM-LOGO.png" alt="logo ethereum">
-            </div>
+        <div class="col-xs-offset-4 col-xs-4 col-lg-offset-2 col-lg-2">
+            <input type="number" value="<?php echo $btcJson->eur; ?>" name="currValue">
         </div>
-        <div class="nbsp"></div>
-        <div class="row qrcode col-lg-12 center">
-            <div class="row center qrcode-text">You could also donate <span class="glyphicon glyphicon-bitcoin"></span> here :D</div>
-            <div class="row"><img class="qrcode-img" src="./images/qrcode.jpg" alt="public key"></div>
-            <div class="row"><img src="./images/vegetacat.gif" alt="" width="50"></div>
-        <script type="text/javascript" src="includes/js/jquery-2.2.0.min.js"></script>
-        <script>
-                var eurtobtc = $('.data').data('eurtobtc');
-                var usdtobtc = $('.data').data('usdtobtc');
-                var btctoeth = $('.data').data('btctoeth');
+        <div class="col-xs-offset-4 col-xs-4  col-lg-2">=</div>
+        <div class="col-xs-offset-4 col-xs-4  col-lg-2">
+            <input type="number" name="btcValue" value="1">
+        </div>
+        <div class="col-xs-offset-4 col-xs-4  col-lg-2"></div>
+        <div class="col-xs-offset-4 col-xs-4">
+            <select name="cryptoCurrency">
+                <option value="btc" selected >BTC</option>
+                <option value="eth" >ETH</option>
+            </select>
+        </div>
+    </section>
 
-            $('#input1').keyup(function(){
-                var currency = $('#currency').val();
-                var source = $('.data').data(currency);
-                var number = $(this).val();
-                var result = number*source;
+    <section id="footer">
+        <div class="copyright">
+            <ul class="menu">
+                <li>&copy; Yaiba782. All rights reserved.</li><li>Design: <a href="http://html5up.net">HTML5 UP</a></li>
+                <li>Data : coinmarketcap-nexuist.rhcloud.com/api</li>
+            </ul>
+        </div>
+    </section>
+</html>
 
-                $('#input2').val(result);
 
-                if ('eurtobtc' == currency){
-                    var eth = (number/$('.data').data('eurtoeth'));
 
-                    $('#input4').val(eth.toFixed(3));
-                    $('#valueIcon').removeClass();
-                    $('#valueIcon').addClass('glyphicon-euro glyphicon');
-                }else{
-                    var eth = (number/$('.data').data('usdtoeth'));
-
-                    $('#input4').val(eth.toFixed(3));
-                    $('#valueIcon').removeClass();
-                    $('#valueIcon').addClass('glyphicon-usd glyphicon');
-                }
-
-            });
-            $('#input2').keyup(function(){
-                var currency = $('#currency').val()
-                var number = $(this).val();
-                var result = parseFloat(number/($('.data').data(currency)));
-
-                $('#input4').val( (number/btctoeth).toFixed(3) );
-                $('#input1').val(result.toFixed(2));
-            });
-            $('#input4').keyup(function(){
-                var currency = $('#currency').val()
-                var number = $(this).val();
-
-                $('#input2').val((number*btctoeth));
-
-                if ('eurtobtc' == currency){
-                    var eth = (number*$('.data').data('eurtoeth'));
-                    $('#input1').val(eth.toFixed(2));
-                }else{
-                    var eth = (number*$('.data').data('usdtoeth'));
-                    $('#input1').val(eth.toFixed(2));
-                }
-
-            });
-
-        </script>
-    </body>
-</html>*/
+<?
+}else{
+    ?>
+    WIP !
+<?php } ?>
